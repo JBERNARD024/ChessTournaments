40bd72e7bc7a9deb0488316b6f4ddc07c2c93007
pedro almeida <pedro_msca2002@hotmail.com>
2023-07-10 03:06:09 +0100

    Acrescentado Controllers que retornam Json, para se poder conectart API

---
+++ ChessTournaments/Controllers/EquipasJsonController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ChessTournaments.Data;
+using ChessTournaments.Models;
+
+namespace ChessTournaments.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EquipasJsonController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EquipasJsonController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/EquipasJson
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Equipa>>> GetEquipa()
+        {
+          if (_context.Equipa == null)
+          {
+              return NotFound();
+          }
+            return await _context.Equipa.ToListAsync();
+        }
+
+        // GET: api/EquipasJson/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Equipa>> GetEquipa(int id)
+        {
+          if (_context.Equipa == null)
+          {
+              return NotFound();
+          }
+            var equipa = await _context.Equipa.FindAsync(id);
+
+            if (equipa == null)
+            {
+                return NotFound();
+            }
+
+            return equipa;
+        }
+
+        // PUT: api/EquipasJson/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutEquipa(int id, Equipa equipa)
+        {
+            if (id != equipa.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(equipa).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EquipaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/EquipasJson
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Equipa>> PostEquipa(Equipa equipa)
+        {
+          if (_context.Equipa == null)
+          {
+              return Problem("Entity set 'ApplicationDbContext.Equipa'  is null.");
+          }
+            _context.Equipa.Add(equipa);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetEquipa", new { id = equipa.Id }, equipa);
+        }
+
+        // DELETE: api/EquipasJson/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEquipa(int id)
+        {
+            if (_context.Equipa == null)
+            {
+                return NotFound();
+            }
+            var equipa = await _context.Equipa.FindAsync(id);
+            if (equipa == null)
+            {
+                return NotFound();
+            }
+
+            _context.Equipa.Remove(equipa);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool EquipaExists(int id)
+        {
+            return (_context.Equipa?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
--- ChessTournaments/Program.cs
+++ ChessTournaments/Program.cs
@@ -1,6 +1,8 @@
 using ChessTournaments.Data;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,23 +11,29 @@ var builder = WebApplication.CreateBuilder(args);
  * ***************************************** */
 
 
-// indica��o de onde est� a Base de Dados
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("A base de dados referenciada pela Connection string 'DefaultConnection' n�o est� a funcionar.");
-// instru��es para adicionar o servi�o de acesso � BD (neste caso, SQL Server)
+// Indication of where the database is located
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("The database referenced by the 'DefaultConnection' connection string is not working.");
+// Instructions to add the database access service (in this case, SQL Server)
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-
-
-
-
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews()
+    .AddNewtonsoftJson(options =>
+    {
+        options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+    });
 
 
 var app = builder.Build();
+
+app.UseCors(builder => builder
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .SetIsOriginAllowed(_ => true)
+    .AllowCredentials());
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
